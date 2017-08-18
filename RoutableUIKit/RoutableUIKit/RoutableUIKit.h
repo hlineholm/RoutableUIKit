c10e0b28adc2d0ea943ff78e0b11fba46561dd1e
Henrik Lineholm <henrik.lineholm@icloud.com>
2017-08-18 10:01:19 +0200

    Add Cocoa Touch Framework project

---
+++ RoutableUIKit/RoutableUIKit/RoutableUIKit.h
@@ -0,0 +1,19 @@
+//
+//  RoutableUIKit.h
+//  RoutableUIKit
+//
+//  Created by Lineholm, Henrik on 2017-08-18.
+//  Copyright © 2017 RouteableUIKit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RoutableUIKit.
+FOUNDATION_EXPORT double RoutableUIKitVersionNumber;
+
+//! Project version string for RoutableUIKit.
+FOUNDATION_EXPORT const unsigned char RoutableUIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RoutableUIKit/PublicHeader.h>
+
+
