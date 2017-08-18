bd04b195dec20a0055c9565145ad1d77682c7e87
Henrik Lineholm <henrik.lineholm@icloud.com>
2017-08-18 15:32:19 +0200

    Refactor NavigationController

--- RoutableUIKit/RoutableUIKit/NavigationController.swift
+++ RoutableUIKit/RoutableUIKit/NavigationController.swift
@@ -5,79 +5,76 @@
 //  Created by Lineholm, Henrik on 2017-08-18.
 //  Copyright © 2017 RouteableUIKit. All rights reserved.
 //
+//  Based on https://github.com/ReSwift/ReSwift-Router/pull/74
+
 
 import UIKit
 
-// dummy view controller returned when popViewController is canceled
+/**
+ A dummy view controller returned when popViewController is canceled
+ */
 public final class PopWasIgnored: UIViewController {}
+
 
 open class NavigationController: UINavigationController {
     
     fileprivate var isSwipping: Bool = false
-    
-    // indicates that backButton was pressed when set to false
     fileprivate var isPerformingPop: Bool = false
     
-    override open func viewDidLoad() {
-        super.viewDidLoad()
-        self.interactivePopGestureRecognizer?.addTarget(self, action: #selector(NavigationController.handlePopSwipe))
-        self.delegate = self
-    }
-    
-    override open func popViewController(animated: Bool) -> UIViewController? {
-        
-        // when swipping we are discarding all subsequent popViewController calls
-        guard !self.isSwipping else {
-            return PopWasIgnored()
-        }
-        
-        self.isPerformingPop = true
-        return super.popViewController(animated: animated)
-    }
-    
-    // will be called after popViewController call
     func handlePopSwipe(){
         self.isSwipping = true
     }
-    
-    /// should be overriden
-    /// normally you should dispatch SetRouteAction here
     open func popRoute(){
-        print("WARNING: \(#function) should to be overriden")
+        print("WARNING: \(#function) has not been implemented, put your custom routing here!")
+    }
+    
+    // MARK: UINavigationController
+    
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        self.interactivePopGestureRecognizer?.addTarget(
+            self,
+            action: #selector(NavigationController.handlePopSwipe)
+        )
+        self.delegate = self
+    }
+    override open func popViewController(animated: Bool) -> UIViewController? {
+        guard !self.isSwipping else { return PopWasIgnored() }
+        self.isPerformingPop = true
+        return super.popViewController(animated: animated)
     }
 }
 
 extension NavigationController: UINavigationControllerDelegate {
-    
-    public func navigationController(_ navigationController: UINavigationController, didShow viewController: UIViewController, animated: Bool) {
+    public func navigationController(
+        _ navigationController: UINavigationController,
+        didShow viewController: UIViewController,
+        animated: Bool) {
+        
         self.isSwipping = false
     }
 }
 
 extension NavigationController: UINavigationBarDelegate {
-    
-    // if overriden navigationController popViewController won't be called before this method
-    // on back button press
-    // isPerformingPop will be false here in this case
-    public func navigationBar(_ navigationBar: UINavigationBar, shouldPop item: UINavigationItem) -> Bool {
+    public func navigationBar(
+        _ navigationBar: UINavigationBar,
+        shouldPop item: UINavigationItem) -> Bool {
+        
         defer { isPerformingPop = false }
         
-        // changeRoute is performed:
-        // 1. back button was pressed
-        // 2. pop swipe was triggerred
         if !isPerformingPop || self.isSwipping {
             self.popRoute()
         }
         
-        // don't remove the navigationItem if navigationController
-        // is not going to be popped
         return isPerformingPop
     }
 }
 
 extension UINavigationController {
-    
-    open func pushViewController(_ viewController: UIViewController, animated: Bool, completion: @escaping () -> Void) {
+    open func pushViewController(
+        _ viewController: UIViewController,
+        animated: Bool,
+        completion: @escaping () -> Void) {
         
         self.pushViewController(viewController, animated: animated)
         
@@ -88,8 +85,9 @@ extension UINavigationController {
         
         coordinator.animate(alongsideTransition: nil) { _ in completion() }
     }
-    
-    open func popViewController(animated: Bool, completion: @escaping () -> Void) -> UIViewController? {
+    open func popViewController(
+        animated: Bool,
+        completion: @escaping () -> Void) -> UIViewController? {
         
         let popped = self.popViewController(animated: animated)
         
@@ -99,6 +97,7 @@ extension UINavigationController {
         }
         
         coordinator.animate(alongsideTransition: nil) { _ in completion() }
+        
         return popped
     }
 }
